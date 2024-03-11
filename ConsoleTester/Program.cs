63c4f3eeccd2518587481eb09fedcc6f45ea95b5
Tim Dams <dams.tim@telenet.be>
2024-03-11 14:35:38 +0100

    Add project files.

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //https://github.com/timdams/GitbookMD_to_LeanPubMarkua/blob/main/Program.cs
+
+
+        }
+    }
+}
